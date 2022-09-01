508edd0c7341affa10266fa30099620705aca831
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-09-01 22:01:50 +0300

    Refactored

--- Assets/Scripts/Citizen/CitizenRotator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CitizenRotator : MonoBehaviour
-{
-    public void Rotate(Vector3 diraction)
-    {
-        transform.rotation = Quaternion.LookRotation(diraction);
-    }
-}
